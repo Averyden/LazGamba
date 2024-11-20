6e2336f794d951e2de11a692cb00297bb7090282
Averyden <igo408gt@gmail.com>
2024-11-20 20:56:57 +0100

    create dictionary containing images that are relevant

---
+++ src/scripts/handlers/gambaHandler.ts
@@ -0,0 +1,7 @@
+const images = [
+    {name: "loss", path: "assets/img/GAMBA imgs/loss.webp"}, // User didnt win anything.
+    {name: "spinning", path: "assets/img/GAMBA imgs/speen.webp"}, // for when the user has pressed the button and we are calculating the chances of a win. 
+    {name: "win", path: "assets/img/GAMBA imgs/win.png"}, // static win image because paint.net cant make gifs!
+    {name: "noMoney", path: "assets/img/GAMBA imgs/noMoreMoney.webp"} // User lost it all for the day.
+]
+
