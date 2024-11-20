e9df833279e6b1e0cf1a7586a4599818dff71a95
Averyden <igo408gt@gmail.com>
2024-11-20 21:14:26 +0100

    Implement basic logic (needs a bit of refinement)

--- src/scripts/handlers/gambaHandler.ts
+++ src/scripts/handlers/gambaHandler.ts
@@ -5,3 +5,22 @@ const images = [
     {name: "noMoney", path: "assets/img/GAMBA imgs/noMoreMoney.webp"} // User lost it all for the day.
 ]
 
+const gamba = document.getElementById("gambaBtn") as HTMLButtonElement
+
+gamba.addEventListener("click", () => handleGambaCalc())
+
+let pity = 50 // once it hits 50, make it super common.
+
+
+// constants related to gamba logic
+const jackpotNumber = 1500
+
+function handleGambaCalc(): void {
+    let chance = Math.round(Math.random() * 3000)
+
+    console.log(`CHANCE: ${chance}\nPITY: ${pity}\nHIGHRANGE: ${jackpotNumber*pity}\nLOWRANGE: ${jackpotNumber/pity}`)
+    if (chance >= (jackpotNumber*pity) && chance <= (jackpotNumber/pity)) {
+        console.log("WINNER") // we are going to implement images later.
+    } // should give us a range that makes sense?
+
+}
