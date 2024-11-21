29a071e00793def4c8cad83923bbc6db90303eee
averyden <igo408gt@gmail.com>
2024-11-21 08:06:43 +0100

    Add new image

--- src/scripts/handlers/gambaHandler.ts
+++ src/scripts/handlers/gambaHandler.ts
@@ -2,13 +2,15 @@ const images = [
     {name: "loss", path: "assets/img/GAMBA imgs/loss.webp"}, // User didnt win anything.
     {name: "spinning", path: "assets/img/GAMBA imgs/speen.webp"}, // for when the user has pressed the button and we are calculating the chances of a win. 
     {name: "win", path: "assets/img/GAMBA imgs/win.png"}, // static win image because paint.net cant make gifs!
-    {name: "noMoney", path: "assets/img/GAMBA imgs/noMoreMoney.webp"} // User lost it all for the day.
+    {name: "noMoney", path: "assets/img/GAMBA imgs/noMoreMoney.webp"}, // User lost it all for the day.
+    {name: "waiting", path: "assets/img/GAMBA imgs/waiting.webp"}
 ]
 
 const gamba = document.getElementById("gambaBtn") as HTMLButtonElement
 
 gamba.addEventListener("click", () => handleGambaCalc())
 
+//TODO: implement a sort of pity system.
 
 // constants related to gamba logic
 const jackpotNumber = 1500
