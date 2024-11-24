91e65be2bbb6f1655ac24173a8ec94c10c30343e
Averyden <igo408gt@gmail.com>
2024-11-24 14:09:31 +0100

    declutter.

--- src/scripts/handlers/gambaHandler.ts
+++ src/scripts/handlers/gambaHandler.ts
@@ -1,42 +1,5 @@
 const pricePerGamba = 50
 
-initCoins()
-
-const lblCoins = document.getElementById("coinLabel") as HTMLDivElement
-
-const updateCoinDisplay = (): void => {
-    if (lblCoins) {
-        lblCoins.innerText = `L-coins: ${lCoins}`
-    }
-}
-
-updateCoinDisplay()
-
-let gambaMessages: any = {}
-
-const loadGambaMessages = async () => {
-    try {
-        const response = await fetch("src/dictionaries/mainGambaDictionaries.json");
-        const text = await response.text();  
-        console.log('Raw Response:', text);  
-        gambaMessages = JSON.parse(text);
-        console.log('Parsed Messages:', gambaMessages);
-    } catch (error) {
-        console.error('Error loading or parsing JSON:', error);
-    }
-};
-
-
-loadGambaMessages();
-
-const images = [
-    {name: "loss", path: "assets/img/GAMBA imgs/loss.webp"}, // User didnt win anything.
-    {name: "spinning", path: "assets/img/GAMBA imgs/speen.webp"}, // for when the user has pressed the button and we are calculating the chances of a win. 
-    {name: "win", path: "assets/img/GAMBA imgs/win.png"}, // static win image because paint.net cant make gifs!
-    {name: "noMoney", path: "assets/img/GAMBA imgs/noMoreMoney.webp"}, // User lost it all for the day.
-    {name: "waiting", path: "assets/img/GAMBA imgs/waiting.webp"}
-]
-
 
 
 const gamba = document.getElementById("gambaBtn") as HTMLButtonElement
