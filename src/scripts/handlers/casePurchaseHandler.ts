ccdd0db4f39d1c12fac134486956a368f6a415c9
Averyden <igo408gt@gmail.com>
2024-12-25 16:26:02 +0100

    introduce data handling for what cases are unlocked

---
+++ src/scripts/handlers/casePurchaseHandler.ts
@@ -0,0 +1,32 @@
+// For making sure the case is purchased before use
+//! Format for savedata, it just uses the case IDs and we run checks
+
+
+const fetchUnlockedCases = (): number[] => {
+    const rawData = localStorage.getItem("unlockedCases")
+    if (!rawData) return []
+
+    try {
+        const parsedData = JSON.parse(rawData)
+        if (Array.isArray(parsedData)) {
+            return parsedData
+        } else if (typeof parsedData === "object" && parsedData !== null) {
+            return [parsedData.gId]
+        } else {
+            console.error("Unexpected data format in unlockedCases:", parsedData)
+            return []
+        }
+    } catch (err) {
+        console.error("Failed to parse unlockedCases:", err)
+        return []
+    }
+}
+
+const saveUnlocked = (caseIds: any): void => {
+    localStorage.setItem("unlockedCases", JSON.stringify(caseIds));
+};
+
+const isGambaUnlocked = (gId: number): boolean => {
+    const unlockedCases = fetchUnlockedCases()
+    return unlockedCases.includes(gId)
+}
