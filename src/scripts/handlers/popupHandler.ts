2b0e63a24e0eabf0e3be39c65081c504c4f16292
Averyden <igo408gt@gmail.com>
2025-01-16 21:04:28 +0100

    Rewrite the original notesu popup code

--- src/scripts/handlers/popupHandler.ts
+++ src/scripts/handlers/popupHandler.ts
@@ -1,2 +1,73 @@
 //! Rewrite of the notesu popup logic because that fucking sucked
 
+type promptTypes = "caseInfo" | "error" 
+
+interface IPopupConfig {
+    title: string
+    confirmText: string
+    onConfirm: () => void
+}
+
+class Popup {
+    private container: HTMLElement
+    private titleElement: HTMLElement
+    private messageElement: HTMLElement
+    private confirmButton: HTMLButtonElement
+    private cancelButton: HTMLButtonElement
+    private config: Record<promptTypes, IPopupConfig>
+
+    constructor(container: string) {
+        this.container =  document.getElementById(container) as HTMLElement
+        this.titleElement = this.container.querySelector("#popupTitle") as HTMLElement
+        this.messageElement = this.container.querySelector("#popupText") as HTMLElement
+        this.confirmButton = this.container.querySelector("#btnConfirm") as HTMLButtonElement
+        this.cancelButton = this.container.querySelector("#btnCancel") as HTMLButtonElement
+
+        this.cancelButton.addEventListener("click", () => this.hide())
+
+        //This is still a messy way to do it, but infinitely better than whatever the fuck i was doing with notesu
+        this.config = {
+            "caseInfo": {
+                title: "Stats for: {}",
+                confirmText: "OK",
+                onConfirm: this.hide
+            },
+            "error": {
+                title: "Error",
+                confirmText: "OK",
+                onConfirm: this.hide
+            },
+        }
+    }
+
+    private errorCodes: Record<string, string> = {
+        "unknownType": "1x5638",
+        "unknownConfig": "1x5861", 
+    }
+
+    public show(type: promptTypes, message: string = "No message provided.") {
+        let config = this.config[type]
+        if (!config) {
+            const errCode = this.errorCodes["unknownType"]
+            message = `Unknown popup type "${type}"\n(error ${errCode})`
+            config = this.config["error"]
+        }
+
+        this.titleElement.textContent = config.title
+        this.messageElement.textContent = message 
+        this.confirmButton.textContent = config.confirmText
+
+        const newConfirmButton = this.confirmButton.cloneNode(true) as HTMLButtonElement
+        this.confirmButton.replaceWith(newConfirmButton)
+        this.confirmButton = newConfirmButton
+        this.confirmButton.addEventListener("click", config.onConfirm.bind(this))
+
+        this.container.classList.add("visible")
+    }
+
+
+
+    public hide() {
+        this.container.classList.remove("visible")
+    }
+}
