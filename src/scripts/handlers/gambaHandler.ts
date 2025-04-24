06ab1b3131f1106d08fa4982280b19190eb816bc
Averyden <igo408gt@gmail.com>
2025-04-24 16:27:19 +0200

    Replace activeCase with curCase in very specific places, so we still charge the user, and reward them when they win, isntead of a free spin AND multiplying with 0

--- src/scripts/handlers/gambaHandler.ts
+++ src/scripts/handlers/gambaHandler.ts
@@ -50,7 +50,7 @@ class GambaHandler {
     
         let timeOutCancel = false;
     
-        if (!adjustCoins(-activeCase.cost)) {
+        if (!adjustCoins(this.curCase.cost)) {
             gambaStatus.innerHTML = "HAH you're poor! come back tomorrow.";
             gambaImg.src = images.find((img) => img.name === "noMoney")!.path;
             timeOutCancel = true;
@@ -104,7 +104,7 @@ class GambaHandler {
     
             if (gambaWin) {
                 gambaStatus.innerHTML = getRanMessage("win");
-                adjustCoins(activeCase.cost * activeCase.winMult);
+                adjustCoins(this.curCase.cost * activeCase.winMult);
                 updateCoinDisplay();
             } else {
                 gambaStatus.innerHTML = getRanMessage("loss");
