3815fdf370ce2ba8fd8675caa47febdd58a61c81
Averyden <igo408gt@gmail.com>
2025-04-25 14:46:39 +0200

    Return a shallow copy of the heavenly case

--- src/scripts/handlers/rareOccurancesHandler.ts
+++ src/scripts/handlers/rareOccurancesHandler.ts
@@ -1,11 +1,11 @@
 const maybeInjectHeavenlyCase = (currentCase: any): any => {
-    const heavenlyChance = 0.04
+    const heavenlyChance = 0.5
     const rng = Math.random()
 
     if (rng < heavenlyChance) {
         const heavenly = gambaCases.find((gCase: any) => gCase.gId === 9999)
         if (heavenly) {
-            return heavenly
+            return { ...heavenly, cost: currentCase.cost }
         }
     }
 
