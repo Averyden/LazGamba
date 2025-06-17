fe3258d16968507ff5090e1aa7dbb61ae5088628
Averyden <igo408gt@gmail.com>
2025-06-17 13:14:31 +0200

    Introduce shitty hack to fix color not returning to normal

--- src/scripts/helpers/rainbowHelper.ts
+++ src/scripts/helpers/rainbowHelper.ts
@@ -6,6 +6,10 @@ function rainbow() {
       (el as HTMLElement).style.color = getRandomColor();
     });
   } else {
+    //Shitty hack, but i honestly cannot be bothered to figure out a dynamic way to fix it.
+    //And yes... we are unfortunately hard coding an element here.
+    const namelbl = document.getElementById("caseName") as HTMLHeadingElement;
+    namelbl.style.color = "#FFF";
     return;
   }
 }
