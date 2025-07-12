67f57dcc9d68c58322582f0a953d63ab726596f1
Averyden <igo408gt@gmail.com>
2025-07-12 17:51:11 +0200

    Move more case related globals over

--- src/scripts/important/globals.ts
+++ src/scripts/important/globals.ts
@@ -3,3 +3,14 @@
 export let gambaCases: any[] = [];
 export let heavenCase: any;
 export let cachedID: number;
+
+export let cachedSelectedCase: {
+  gId: number;
+  price: number;
+  name: string;
+} | null = null;
+
+export let selectedGambaCase: any = null;
+export let caseID: number = 0;
+
+export const popup = new Popup("popupContainer");
