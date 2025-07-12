03e2e17d18790e740f8f1f0e6b332226f4facdcb
Averyden <igo408gt@gmail.com>
2025-07-12 20:25:33 +0200

    create init.ts

---
+++ src/scripts/main.ts
@@ -0,0 +1,12 @@
+import "./handlers/casePurchaseHandler";
+import "./handlers/currencyHandler";
+import "./handlers/dailyResetHandlerandler";
+import "./handlers/gambaHandler";
+import "./handlers/popupHandler";
+import "./handlers/rareOccurancesHandlerHandler";
+
+import "./helpers/rainbowHelper";
+
+import "./important/config";
+import "./important/globals";
+import "./important/init";
