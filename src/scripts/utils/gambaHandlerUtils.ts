d73c2e745a7b1583244f1f8ef075a60e8175715a
Averyden <igo408gt@gmail.com>
2025-07-13 02:03:32 +0200

    add logic to be able to finally do the calculations

--- src/scripts/utils/gambaHandlerUtils.ts
+++ src/scripts/utils/gambaHandlerUtils.ts
@@ -1,3 +1,19 @@
+import { utilityPopup } from "./emergencyUtils";
+import { getGambaHandler } from "../helpers/gambaHandlerInstance";
+
 export const gambaButton = document.getElementById(
   "gambaBtn"
 ) as HTMLButtonElement;
+
+gambaButton.addEventListener("click", () => {
+  const handler = getGambaHandler();
+  if (!handler) {
+    utilityPopup.show(
+      "error",
+      `Error when handling gamba calculations, handler is not yet initialized. <br>(error ${utilityPopup.errorCodes["handlerNotInitWhenHandlingCalc"]})`
+    );
+    console.error("Handler not initialized yet.");
+    return;
+  }
+  handler.handleGambaCalc();
+});
